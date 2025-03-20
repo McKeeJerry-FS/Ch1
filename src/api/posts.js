36f9d6b584c4bd3949894695919d137acbf3c6d6
McKeeJerry-FS <jerry.mckee.jr0882@gmail.com>
2025-03-19 21:49:44 -0400

    working on hooking up the backend

---
+++ src/api/posts.js
@@ -0,0 +1,24 @@
+export const getPosts = async (queryParams) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/posts?` +
+      new URLSearchParams(queryParams),
+  )
+  return await res.json()
+}
+
+export const createPost = async (post) => {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(post),
+  })
+  return await res.json()
+}
+
+// export async function getPosts() {
+//   const response = await fetch('/api/v1/posts');
+//   if (!response.ok) {
+//     throw new Error('Network response was not ok');
+//   }
+//   return response.json();
+// }
